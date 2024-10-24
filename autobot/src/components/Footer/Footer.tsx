9c281b2ee24c7619d5b07ce835d5a30f9d8d6aba
SunaUezuri <wesley.jane.santos@gmail.com>
2024-10-24 20:34:53 -0300

    Footer concluído juntamente de sua responsividade.

--- autobot/src/components/Footer/Footer.tsx
+++ autobot/src/components/Footer/Footer.tsx
@@ -3,31 +3,47 @@ import logoKitsune from "@/img/logo-kitsune-tech.png";
 import { FaInstagram as InstagramLogo, FaWhatsappSquare as WhatsapLogo } from "react-icons/fa";
 import { FaFacebookSquare as FacebookLogo } from "react-icons/fa";
 
-export default function Footer(){
-    return(
-        <footer className="w-full text-footerColor">
-            <div>
-                <div className="bg-zinc-900 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+export default function Footer() {
+  return (
+    <footer className="w-full text-footerColor">
+      <div>
+        <div className="bg-zinc-900 grid grid-cols-1 md:grid-cols-2 p-4">
 
-                    <div>
-                        <span>
-                            <Image 
-                                src={logoKitsune} 
-                                alt="Logo Kitsune Tech"
-                                className="w-1/2"/>
-                        </span>
-                        <p className="gap-4 mb-6">Em prol da agilidade.</p>
+          <div className="flex flex-col items-start">
+            <span>
+              <Image
+                src={logoKitsune}
+                alt="Logo Kitsune Tech"
+                className="w-40 md:w-1/4 block"
+              />
+            </span>
+            <p className="gap-4 mb-6 text-sm md:text-base">Em prol da agilidade.</p>
 
-                        <div className="flex">
-                            <a href="#"><InstagramLogo className="text-instagramColor text-3xl"/></a>
-                            <a href="#"><FacebookLogo className="text-facebookColor text-3xl"/></a>
-                            <a href="#"><WhatsapLogo className="text-whatsappColor text-3xl"/></a>
-                        </div>
-                        
-                    </div>
-
-                </div>
+            <div className="flex space-x-2">
+              <a href="#"><InstagramLogo className="text-instagramColor text-lg md:text-3xl" /></a>
+              <a href="#"><FacebookLogo className="text-facebookColor text-lg md:text-3xl" /></a>
+              <a href="#"><WhatsapLogo className="text-whatsappColor text-lg md:text-3xl" /></a>
             </div>
-        </footer>
-    )
+          </div>
+
+          <div className="flex justify-between">
+            <ul className="footerList items-center">
+              <li className="font-bold mb-2">Blog</li>
+              <li><a href="#">Tech</a></li>
+              <li><a href="#">Notícias</a></li>
+              <li><a href="#">Música</a></li>
+            </ul>
+
+            <ul className="footerList items-center">
+              <li className="font-bold mb-2">Produtos</li>
+              <li><a href="#">App</a></li>
+              <li><a href="#">Desktop</a></li>
+              <li><a href="#">Cloud</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </footer>
+  );
 }
