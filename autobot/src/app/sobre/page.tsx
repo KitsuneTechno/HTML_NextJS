104a7a281cac0b1235de58aeaaaa6196cae9e057
SunaUezuri <wesley.jane.santos@gmail.com>
2024-10-25 15:59:32 -0300

    Página Sobre concluída.

---
+++ autobot/src/app/sobre/page.tsx
@@ -0,0 +1,64 @@
+export default function Sobre(){
+    return(
+        <div className="aboutContainer">
+            <h1>Seja Bem-Vindo à Kitsune Tech!</h1>
+
+            <h3>Sobre nós</h3>
+
+            <p>
+                Na Kitsune Tech, estamos empenhados em redefinir a maneira como as
+                pessoas interagem com a tecnologia no seu dia a dia. Nós acreditamos
+                no poder da inovação para transformar o mundo ao nosso redor, e é por
+                isso que desenvolvemos soluções inteligentes e intuitivas para
+                facilitar a vida das pessoas.
+            </p>
+
+            <h3>Nossa Missão</h3>
+
+            <p>
+                Nossa missão é criar produtos e serviços que simplifiquem e aprimorem
+                a experiência das pessoas com a tecnologia. Queremos tornar as tarefas
+                cotidianas mais fáceis, eficientes e agradáveis, permitindo que nossos
+                usuários se concentrem no que realmente importa para eles.
+            </p>
+
+            <h3>O Que Fazemos?</h3>
+
+            <p>
+                Na Kitsune Tech, desenvolvemos aplicativos e plataformas digitais
+                inovadoras que abordam desafios do mundo real. Desde soluções de
+                assistência automotiva até ferramentas de gestão financeira pessoal,
+                estamos constantemente buscando maneiras de tornar a vida mais simples
+                e mais conectada.
+            </p>
+
+            <h3>Por Que Escolher a Kitsune Tech?</h3>
+
+            <p>
+                Inovação Constante: Estamos sempre buscando novas maneiras de melhorar
+                e expandir nossas soluções para atender às necessidades em evolução
+                dos nossos usuários.
+            </p>
+
+            <p>
+                Qualidade e Confiabilidade: Nosso compromisso com a excelência garante
+                que nossos produtos e serviços atendam aos mais altos padrões de
+                qualidade e confiabilidade.
+            </p>
+
+            <p>
+                Foco no Usuário: Colocamos os usuários no centro de tudo o que
+                fazemos, garantindo que nossas soluções sejam intuitivas, acessíveis e
+                realmente úteis em suas vidas.
+            </p>
+
+            <p>
+                Paixão pela Tecnologia: Somos apaixonados por tecnologia e pela
+                capacidade que ela tem de transformar o mundo para melhor. Essa paixão
+                impulsiona tudo o que fazemos na Kitsune Tech. Junte-se a nós na
+                jornada para simplificar e aprimorar a experiência das pessoas com a
+                tecnologia.
+            </p>
+        </div>
+    )
+}
