bc38308e0f4be4736a60382ee1e8d6311077250c
SunaUezuri <wesley.jane.santos@gmail.com>
2024-10-25 12:26:55 -0300

    Home page concluída adicionando os últimos itens informativos e a imagem do celular.

--- autobot/src/components/Section/Section.tsx
+++ autobot/src/components/Section/Section.tsx
@@ -1,3 +1,5 @@
+import Celular from "@/img/celular.png"
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Section(){
@@ -7,9 +9,14 @@ export default function Section(){
 
             <div className="flex flex-row items-start justify-start md:items-center md:justify-center">
                 <ol className="list-none flex flex-col gap-4">
-                    <li className="styleButton"><Link href={"cliente/login"}></Link></li>
-
+                    <li className="styleButton"><Link href={"cliente/login"}>Dê o primero passo!</Link></li>
+                    <li className="item">Dê o primeiro passo.</li>
+                    <li className="item">Descreva sua Solicitação.</li>
+                    <li className="item">Receba Ajuda ou Informações</li>
+                    <li className="item">Volte Sempre!</li>
                 </ol>
+
+                <Image src={Celular} alt="Imagem de celular" className="w-36 md:w-44"/>
             </div>
         </section>
     )
